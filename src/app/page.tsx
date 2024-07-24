9211524b57752e6394ac6f94f4a9c69641e668e3
Josh <karta.josh.natin@gmail.com>
2024-07-24 18:23:49 -0300

    feat: landing page hero image and features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   BrainCogIcon,
   EyeIcon,
@@ -51,7 +52,7 @@ const features = [
 
 export default function Home() {
   return (
-    <main className="flex-1 p-2 lg:p-4 bg-gradient-to-bl from-white to-neutral-300 pt-4">
+    <main className="flex-1 overflow-y-scroll p-2 lg:p-4 bg-gradient-to-bl from-white to-neutral-300 pt-4">
       <Hero />
     </main>
   );
@@ -81,7 +82,38 @@ function Hero() {
             <Link href={"/dashboard"}>Get Started</Link>
           </Button>
         </div>
+
+        <div className="relative overflow-hidden pt-16">
+          <div className="mx-autp max-w-7xl px-6 lg:px-8">
+            <Image src={"https://i.imgur.com/VciRSTI.jpeg"} alt="hero" width={2432} height={1442} className="mb-[-0%] rounded-lg shadow-2xl  ring-1 ring-gray-900/10" />
+            <div aria-label="true" className="relative">
+              <div className="bottom-0 absolute -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+            </div>
+          </div>
+        </div>
+
+        <Features />
       </div>
     </div>
   );
+}
+
+function Features() {
+  return (
+    <div className="lg:mt-20 mt-14 mx-auto grid max-2-xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+      {features.map((feature) => (
+        <Card key={feature.name}>
+          <CardHeader>
+            <CardTitle className="flex items-center gap-2 text-base">
+              <feature.icon aria-label="trues" className="w-5 h-5 text-green-500" />
+              {feature.name}
+            </CardTitle>
+            <CardDescription>
+              {feature.description}
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      ))}
+    </div>
+  )
 }
