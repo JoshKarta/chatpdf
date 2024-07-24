d87ee4380f1e3a53bf4868e13000544da2c4b2fb
Josh Karta <josh.kartadiwirja@kabinetpresident.gov.sr>
2024-07-24 17:47:15 -0300

    feat: start with hero of landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={cn(
+          inter.className,
+          "min-h-screen h-screen overflow-hidden flex flex-col"
+        )}
+      >
         <ClerkProvider>
           <Header />
           {children}
